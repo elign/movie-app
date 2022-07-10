a539153162322df9869812b1b25e7695df692149
Harshit <harshitshivam001@gmail.com>
2022-07-10 17:55:37 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import {useState, useEffect} from 'react';
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from './MovieCard';
+
+//cc20dc56
+
+const API_URL = 'http://www.omdbapi.com?apikey=cc20dc56'
+
+const movie1 = {
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg",
+    "Title": "Superman, Spiderman or Batman",
+    "Type": "movie",
+    "Year": "2011",
+    "imdbID": "tt2084949"
+}
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('BatMan')
+    },[]);
+    return (
+        <div className='app'>
+            <h1>Harshit's Movie Land</h1>
+
+            <div className='search'>
+                <input 
+                    placeholder='Search for Movies'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img
+                    src={SearchIcon}
+                    alt='search'
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0 
+                ?(
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                ) 
+                :(
+                    <div>
+                        <h2>No Movies Found</h2>
+                    </div>
+                )
+            }
+
+        </div>
+    );
+}
+
+export default App;
